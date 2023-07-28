b3eeab94a6b5c1f75c5e240cf80276bb8b81779a
farhan7479 <farhan078609@gmail.com>
2023-07-28 20:41:01 +0530

    update-Json-file

--- models/User.js
+++ models/User.js
@@ -25,10 +25,10 @@ const UserSchema = new mongoose.Schema(
     subscribedUsers: {
       type: [String],
     },
-    fromGoogle: {
-      type: Boolean,
-      default: false,
-    },
+    // fromGoogle: {
+    //   type: Boolean,
+    //   default: false,
+    // },
   },
   { timestamps: true }
 );
